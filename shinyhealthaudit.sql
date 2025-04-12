dc09d8b6520db276b260cb3bf62eb25a856dc705
hyperhop <47045184+hyperhop@users.noreply.github.com>
2025-04-12 18:27:47 +1000

    Add files via upload

--- shinyhealthaudit.sql
+++ shinyhealthaudit.sql
@@ -1,35 +1,54 @@
+USE [master]
+GO
+
+CREATE DATABASE [HealthCheck]
+GO
+
+USE [HealthCheck]
+GO
+
 -- Shiny Health Audit tables
 
-
 CREATE TABLE "users" (
-"id" INT Primary Key IDENTITY,
-"name" varchar(50) NOT NULL,
-"surname" varchar(50) NOT NULL,
-"job title" varchar(50),
-"email" varchar(50)
+	"id" INT Primary Key IDENTITY,
+	"name" varchar(50) NOT NULL,
+	"surname" varchar(50) NOT NULL,
+	"job title" varchar(50),
+	"email" varchar(50)
 )
 ;
 
 INSERT INTO "users"
 values
-('Adam', 'Alpha', 'Worker', 'adam.alpha@shinyhealth.com'),
-('Billy', 'Bald', 'Worker',	'billy.bald@shinyhealth.com'),
-('Cindy', 'Centaur', 'Worker', 'cindy.centaur@shinyhealth.com'),
-('Denise', 'Dragon', 'Worker', 'denise.dragon@shinyhealth.com'),
-('Barnabas', 'Soon', 'Manager',	'barnabas.soon@shinyhealth.com'),
-('Shunnie',	'Xie', 'Manager', 'shunnie.xie@shinyhealth.com')
+	('Adam', 'Alpha', 'Worker', 'adam.alpha@shinyhealth.com'),
+	('Billy', 'Bald', 'Worker',	'billy.bald@shinyhealth.com'),
+	('Cindy', 'Centaur', 'Worker', 'cindy.centaur@shinyhealth.com'),
+	('Denise', 'Dragon', 'Worker', 'denise.dragon@shinyhealth.com'),
+	('Barnabas', 'Soon', 'Manager',	'barnabas.soon@shinyhealth.com'),
+	('Shunnie',	'Xie', 'Manager', 'shunnie.xie@shinyhealth.com')
+;
+
+CREATE TABLE "audit_types" (
+"id" INT PRIMARY KEY IDENTITY,
+"name" varchar(30) NOT NULL,
+"status" varchar(20) Check ("status" in ('active', 'inactive'))
+)
 ;
 
 CREATE TABLE "audits" (
-"id" INT PRIMARY KEY IDENTITY,
-"year" INT PRIMARY KEY NOT NULL,
-"type" INT FOREIGN KEY NOT NULL,
-"start_date" DATETIME,
-"end_date" DATETIME,
-"title" NOT NULL varchar(100), 
-"description" varchar(1500),
-"previous_audit" int FOREIGN KEY
-next_audit: int FOREIGN KEY
-status: varchar(20)
-Check status = (‘Started’, ‘In Progress’, ‘Completed’, ‘Closed’)
+	"id" INT PRIMARY KEY IDENTITY,
+	"year" INT,
+	"type" INT FOREIGN KEY REFERENCES audit_types(id), 
+	"start_date" DATETIME,
+	"end_date" DATETIME,
+	"title" varchar(100) NOT NULL, 
+	"description" varchar(1500),
+	"previous_audit" INT FOREIGN KEY REFERENCES audits(id),
+	"next_audit" INT FOREIGN KEY REFERENCES audits(id),
+	"status" varchar(20) Check ("status" in ('Started', 'In Progress', 'Completed', 'Closed'))
+		
+	)
+;
 
+GO
+ 
